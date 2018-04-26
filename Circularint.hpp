717cd9eddc09e139d99706558ddde8ee79440a22
JosephGolubchik <37150623+JosephGolubchik@users.noreply.github.com>
2018-04-26 13:31:24 +0300

    Create Circularint.hpp

---
+++ Circularint.hpp
@@ -0,0 +1,93 @@
+#include <iostream>
+
+using namespace std;
+
+class CircularInt {
+    
+    int start, end;
+    int curHour;
+    
+    public:
+    
+    CircularInt(int start, int end);
+    CircularInt(int start, int end, int curHour);
+    
+    int getStart();
+    int getEnd();
+    int getCurHour();
+    void getInRange();
+    void setCurHour(int hour);
+    
+    friend ostream& operator<<(ostream& os, const CircularInt& circ);  
+    
+    CircularInt& operator+= (int add);
+    CircularInt& operator+= (CircularInt& circ);
+    CircularInt& operator++ ();
+    CircularInt operator++ (int);
+    int operator+ (CircularInt& circ);
+    
+    CircularInt& operator-= (int sub);
+    CircularInt& operator-= (CircularInt& circ);
+    CircularInt& operator-- ();
+    CircularInt operator-- (int);
+    int operator- ();
+    int operator- (CircularInt& circ);
+    // CircularInt& operator- (int baseHour, CircularInt& circ);
+    
+    CircularInt& operator*= (int mult);
+    CircularInt& operator*= (CircularInt& circ);
+    int operator* (CircularInt& circ);
+    int operator* (int mult);
+    
+    CircularInt& operator/= (int divi);
+    int operator/ (int divi);
+    int operator/ (CircularInt& circ);
+    
+    CircularInt& operator= (CircularInt& circ);
+    CircularInt& operator= (int hour);
+    bool operator== (CircularInt& circ);
+    bool operator== (int a);
+    
+    int operator% (CircularInt& circ);
+    
+    bool operator!= (CircularInt& circ);
+    bool operator!= (int a);
+    bool operator! ();
+    
+    bool operator> (CircularInt& circ);
+    bool operator> (int a);
+    bool operator>= (CircularInt& circ);
+    bool operator>= (int a);
+    bool operator< (CircularInt& circ);
+    bool operator< (int a);
+    bool operator<= (CircularInt& circ);
+    bool operator<= (int a);
+    
+    bool operator&& (CircularInt& circ);
+    bool operator&& (int a);
+    
+    bool operator|| (CircularInt& circ);
+    bool operator|| (int a);
+    
+    int operator~ ();
+    
+    int operator& (CircularInt& circ);
+    int operator& (int a);
+    CircularInt& operator&= (int a);
+    
+    int operator| (CircularInt& circ);
+    int operator| (int a);
+    
+    int operator^ (CircularInt& circ);
+    int operator^ (int a);
+    
+    int operator<< (CircularInt& circ);
+    int operator<< (int shift);
+    int operator>> (CircularInt& circ);
+    int operator>> (int shift);
+};
+
+CircularInt& operator- (int baseHour, CircularInt& circ);
+CircularInt& operator+ (int baseHour, CircularInt& circ);
+CircularInt& operator* (int baseHour, CircularInt& circ);
+CircularInt& operator/ (int baseHour, CircularInt& circ);
